251887f89bbd3590c765807c471a049fa2072156
ryanmoralesaz <ryanmoralesaz@gmail.com>
2022-06-09 09:52:29 -0700

    pushed stafed items

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer Yp09otQMR1ffRuTMfuWFYiNWv5s7NvR1bW4HajkX94qT-wQ57Hzb8HwbwwLbQPj9bVhEpu691ucqZsdKnjcDESR9ha0mKUDy_h05DIXljGcnByhTpz1qwUqaJ_iKYnYx',
+        Authorization: process.env.REACT_APP_API_KEY,
     }
 });
